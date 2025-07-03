ca7548321451125f17148249ba78a1b73ffdf833
rilldata <support@rilldata.com>
2025-07-03 13:55:24 +0530

    Brew formula update for rill version v0.66.6

--- rill.rb
+++ rill.rb
@@ -7,13 +7,22 @@ class Rill < Formula
   homepage "https://github.com/rilldata/rill"
   version "0.66.6"
   license "Apache 2.0"
-  depends_on :linux
+  depends_on :macos
 
-  if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
-    url "https://cdn.rilldata.com/rill/v0.66.6/rill_linux_amd64.zip"
-    sha256 "59a76f7da0b14d49e974db9720d43a27ee8f4d466a17240bbe5bcbc0a3bd3ad7"
-    def install
-      bin.install "rill"
+  url "https://cdn.rilldata.com/rill/v0.66.6/rill_darwin_amd64.zip"
+  sha256 "546ec1ae98ec0f693b333d33d1c9ff913c2dc9ce0973c8c203ac862c677ebd85"
+
+  def install
+    bin.install "rill"
+  end
+
+  if Hardware::CPU.arm?
+    def caveats
+      <<~EOS
+        The darwin_arm64 architecture is not supported for the Rill
+        formula at this time. The darwin_amd64 binary may work in compatibility
+        mode, but it might not be fully supported.
+      EOS
     end
   end
 
